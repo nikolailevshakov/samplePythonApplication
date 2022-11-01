262cdf5738e52c5575804cd08b5da45a3f0f711c
Nikolai Levshakov <nikolai_levshakov@epam.com>
2022-11-01 15:04:16 +0100

    Added second page, css, js (with react and babel)

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello World<\h1>"
+def index(name="User"):
+    name = "Kolya"
+    return render_template("index.html", name=name)
+@app.route("/cat")
+def cat():
+    return render_template("cat.html")
